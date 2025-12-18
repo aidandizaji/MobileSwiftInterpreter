837c86fb38ec0aaf1bc10bb7c79ee08ea0327e54
Aidan <Aidan.dizaji@gmail.com>
2025-12-17 23:21:25 -0500

    Initial Commit

---
+++ MobileSwiftInterpreter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileSwiftInterpreter
+//
+//  Created by Aidan Dizaji on 2025-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileSwiftInterpreter/MobileSwiftInterpreterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileSwiftInterpreterApp.swift
+//  MobileSwiftInterpreter
+//
+//  Created by Aidan Dizaji on 2025-12-17.
+//
+
+import SwiftUI
+
+@main
+struct MobileSwiftInterpreterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
